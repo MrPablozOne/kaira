41e252ddf3d9bdf2825d2b90ff7ec0af91d268aa
Pavel Siemko <pavel.siemko.st@vsb.cz>
2015-01-15 17:42:03 +0100

    Create transition test in simulation without data

--- gui/app.py
+++ gui/app.py
@@ -504,7 +504,7 @@ class App:
             simulation.connect("localhost", port)
 
             #callback create_test, we must know the app->project in simulation
-            simulation.set_callback("create_test", lambda: self.transition_test(self))
+            simulation.set_callback("create_test", lambda w: self.transition_test(w))
 
         simconfig = self.project.get_simconfig()
         if simconfig.parameters_values is None:
